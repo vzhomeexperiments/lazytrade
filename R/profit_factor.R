be67d0ff229cfafe249abf142ad0f085de849548
vzhomeexperiments <vz.home.experiments@gmail.com>
2019-06-01 13:24:52 +0200

    Update profit_factor.R

--- R/profit_factor.R
+++ R/profit_factor.R
@@ -1,25 +1,26 @@
-
-#===============================
-# PROFIT FACTOR FUNCTION
-#===============================
-# function that returns the profit factors of the systems
+#' PROFIT FACTOR FUNCTION
+#'
+#' @param x column vector with profit or loss of the orders for one system
 #
-# x - column vector with profit or loss of the orders for one system
-#     function should calculate profit factor for this vector and return one value also as vector
-# require(magrittr)
-# x <- read_rds('test_data.rds') %>% select(X5) %$% X5 
-# x <- read_rds('test_data.rds') %>% filter(X5 > 0) %>% select(X5) %$% X5 
-# x <- read_rds('test_data.rds') %>% filter(X5 < 0) %>% select(X5) %$% X5 
-
+#'
+#' @return function should calculate profit factor for this vector and return one value also as vector
+#' @export
+#'
+#' @examples
+#'
+#' # require(magrittr)
+#' x <- read_rds('test_data.rds') %>% select(X5) %$% X5
+#' x <- read_rds('test_data.rds') %>% filter(X5 > 0) %>% select(X5) %$% X5
+#' x <- read_rds('test_data.rds') %>% filter(X5 < 0) %>% select(X5) %$% X5
+#'
 profit_factor <- function(x){
-  
+
+  # calculate profit factor
   res <- sum(x[x>0])/(0.0001+sum(abs(x[x<0])))
-  
+
   # limit profit factor to maximum value of 10 (in case no losses)
-  if(res > 100) res <- 10 
+  if(res > 100) res <- 10
   return(res)
-  
+
   }
-  
-# test this function
-# profit_factor(x)
+
