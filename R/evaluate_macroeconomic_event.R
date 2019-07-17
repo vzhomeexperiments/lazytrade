e7f9188b0f78fa7be9cab1524b6c9368323d8222
vzhomeexperiments <vz.home.experiments@gmail.com>
2019-07-17 20:50:19 +0200

    saving a working version

--- R/evaluate_macroeconomic_event.R
+++ R/evaluate_macroeconomic_event.R
@@ -1,13 +1,30 @@
 #' Function used to evaluate market type situation by reading the file with Macroeconomic Events
 #'
 #' @description Function is reading the content of the file 01_MacroeconomicEvent.csv.
-#' Should the content of the file is 1 then Macro Economic event is present,
-#' all trading robots will not be able to open new orders
+#' Content of the file can be either 1 or 0.
+#' 1 - when Macro Economic event is present, 0 - when it's not.
+#' Function will also read magic number of the trading robots. This is indicated in the file 'Setup.csv'.
+#' Final outcome of the function is the series of files written to the destination directories.
+#' These files will either enable or disable opening of new positions in the trading robots
+#'#'
 #'
-#' @details This function is used exclusively with Market Type recognition system
+#' @details This function is used exclusively with Market Type recognition system.
+#'
+#' Final evaluation will consist in writing a dedicated file with a simple information:
+#'
+#' When Macro economic even is not present:
+#'
+#' "Magic","IsEnabled"
+#' 8139125,1
+#'
+#' or, when Macro economic event is present:
+#'
+#' "Magic","IsEnabled"
+#' 8139125,0
 #'
 #' @param setup_path Path with trading robots magic numbers setup, typically file 'Setup.csv'
-#' @param file_name Name of the file
+#' @param file_name String indicating name of the file where Macroeconomic event is written, typically
+#' "01_MacroeconomicEvent.csv"
 #' @param path_T1 Path of the Terminal 1
 #' @param path_T3 Path of the Terminal 3
 #'
@@ -16,20 +33,18 @@
 #'
 #' @examples
 #'
-#'
-#'
 #' # evaluate data on macroeconomic event (required to start trading)
 #' library(tidyverse)
-#' 
+#'
 #' dir <- tempfile()
-#' 
+#'
 #' evaluate_macroeconomic_event(setup_path = system.file("extdata",
 #'                                                  "Setup.csv",
 #'                                                  package = "lazytrade"),
 #'                              file_name = system.file("extdata",
 #'                                                  "01_MacroeconomicEvent.csv",
 #'                                                  package = "lazytrade"),
-#'                              path_T1 = "",path_T3 = dir)
+#'                              path_T1 = "", path_T3 = dir)
 #'
 #' #unlink("/tmp/*.csv", recursive = TRUE, force = TRUE)
 #'
@@ -76,6 +91,37 @@ evaluate_macroeconomic_event <- function(setup_path, file_name, path_T1, path_T3
         group_by(Magic) %>% select(Magic) %>% mutate(IsEnabled = 1) %>%
         # write commands to disable systems
         writeCommandViaCSV(path_T1)
+
+      # disable trades in T3
+      DF_Setup %>% group_by(Magic) %>%
+        mutate(MagicNumber = Magic + 200, IsEnabled = 1) %>%
+        group_by(MagicNumber) %>%
+        select(MagicNumber, IsEnabled) %>%
+        # write commands to disable systems
+        writeCommandViaCSV(path_T3)
+
+    }
+
+  } else { # test the example
+    #read the file containing a flag (1 will mean that event is present hence no new opened orders)
+    DF_NT <- read_csv(file= file.path(file_name), col_types = "i")
+    #read the table of trading robots in operation
+    DF_Setup <- read_csv(setup_path)
+    ## condition to disable systems
+    if(DF_NT[1,1] == 1) {
+
+      # disable trades in T3
+      DF_Setup %>% group_by(Magic) %>%
+        mutate(MagicNumber = Magic + 200, IsEnabled = 0) %>%
+        group_by(MagicNumber) %>%
+        select(MagicNumber, IsEnabled) %>%
+        # write commands to disable systems
+        writeCommandViaCSV(path_T3)
+
+    }
+
+    ## condition to enable systems
+    if(DF_NT[1,1] == 0) {
 
       # disable trades in T3
       DF_Setup %>% group_by(Magic) %>%
